64c9bd228b6f2f103ea704bb689e4ac1a176efbe
mchl-mccrthy <62691367+mchl-mccrthy@users.noreply.github.com>
2026-07-06 11:05:29 +0200

    Move map plotting to new function (tidying up)

--- src/makeplots.m
+++ src/makeplots.m
@@ -10,43 +10,15 @@ function makeplots(station_clim_var,station_coords,station_time,...
 % Get number of stations
 n_stations = width(station_clim_var);
 
-% Plot map of long-term average of bias-corrected data
-figure()
-contourf(raw_lon,raw_lat,mean(bc_grid_clim_var_yearly,3),100, ...
-    'LineColor','none')
-c = colorbar;
-if strcmp(agg_method,'sum')
-    c.Label.String = [clim_var_long_name ' (' clim_var_units ' year^{-1})'];
-elseif strcmp(agg_method,'mean')
-    c.Label.String = [clim_var_long_name ' (' clim_var_units ')'];
-end
-title('Long-term average')
-ll_ratio = (max(raw_lon,[],'all') - min(raw_lon,[],'all')) ./ ...
-           (max(raw_lat,[],'all') - min(raw_lat,[],'all'));
-formatfigure(gcf,4,4/ll_ratio,2)
-print(gcf, [file_path_figures '/' clim_var_name ...
-    '_long-term_average.png'], '-dpng','-r300');
-
-% Plot map of long-term linear trends of bias-corrected data
-figure()
-contourf(raw_lon,raw_lat,bc_grid_linear_trends,100,'LineColor','none')
-c = colorbar;
-if strcmp(agg_method,'sum')
-    c.Label.String = [clim_var_long_name ' trend (' clim_var_units ' year^{-2})'];
-elseif strcmp(agg_method,'mean')
-    c.Label.String = [clim_var_long_name ' trend (' clim_var_units ' year^{-1})'];
-end
-title('Long-term trend')
-ll_ratio = (max(raw_lon,[],'all') - min(raw_lon,[],'all')) ./ ...
-           (max(raw_lat,[],'all') - min(raw_lat,[],'all'));
-formatfigure(gcf,4,4/ll_ratio,2)
-print(gcf, [file_path_figures '/' clim_var_name ...
-    '_bc_linear_trend.png'], '-dpng','-r300');
+% Make maps of long-term average and linear trends
+plotmaps(raw_lon,raw_lat,bc_grid_clim_var_yearly,...
+    bc_grid_linear_trends,file_path_figures,clim_var_name,...
+    clim_var_long_name,clim_var_units,agg_method);
 
 % Make scatter plot of linear trends
 plottrendcomparison(station_linear_trends,...
     raw_station_linear_trends,bc_station_linear_trends,...
-    file_path_figures,clim_var_name,clim_var_long_name,clim_var_units)
+    file_path_figures,clim_var_name,clim_var_long_name,clim_var_units);
 
 % Make quantile-quantile plots for all stations
 plotallstationsqq( ...
---
+++ src/plotmaps.m
@@ -0,0 +1,39 @@
+% Plot maps of long-term averages and linear trends
+function plotmaps(raw_lon,raw_lat,bc_grid_clim_var_yearly,...
+    bc_grid_linear_trends,file_path_figures,clim_var_name,...
+    clim_var_long_name,clim_var_units,agg_method)
+
+% Plot map of long-term average of bias-corrected data
+figure()
+contourf(raw_lon,raw_lat,mean(bc_grid_clim_var_yearly,3),100, ...
+    'LineColor','none')
+c = colorbar;
+if strcmp(agg_method,'sum')
+    c.Label.String = [clim_var_long_name ' (' clim_var_units ' year^{-1})'];
+elseif strcmp(agg_method,'mean')
+    c.Label.String = [clim_var_long_name ' (' clim_var_units ')'];
+end
+title('Long-term average')
+ll_ratio = (max(raw_lon,[],'all') - min(raw_lon,[],'all')) ./ ...
+           (max(raw_lat,[],'all') - min(raw_lat,[],'all'));
+formatfigure(gcf,4,4/ll_ratio,2)
+print(gcf, [file_path_figures '/' clim_var_name ...
+    '_long-term_average.png'], '-dpng','-r300');
+
+% Plot map of long-term linear trends of bias-corrected data
+figure()
+contourf(raw_lon,raw_lat,bc_grid_linear_trends,100,'LineColor','none')
+c = colorbar;
+if strcmp(agg_method,'sum')
+    c.Label.String = [clim_var_long_name ' trend (' clim_var_units ' year^{-2})'];
+elseif strcmp(agg_method,'mean')
+    c.Label.String = [clim_var_long_name ' trend (' clim_var_units ' year^{-1})'];
+end
+title('Long-term trend')
+ll_ratio = (max(raw_lon,[],'all') - min(raw_lon,[],'all')) ./ ...
+           (max(raw_lat,[],'all') - min(raw_lat,[],'all'));
+formatfigure(gcf,4,4/ll_ratio,2)
+print(gcf, [file_path_figures '/' clim_var_name ...
+    '_bc_linear_trend.png'], '-dpng','-r300');
+
+end
